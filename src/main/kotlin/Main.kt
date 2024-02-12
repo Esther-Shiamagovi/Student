bd796f48853f89efb0e4c641fd0f147824091341
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-02-12 14:17:42 +0300

    added codes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+var name="Esther"
+     println(name)
+    var age=20
+    age=20
+    println(age)
+    var location="Kericho"
+    println(location)
+    val dateOfBirth= 26
+    println(dateOfBirth)
+    val yearOfBirth=2005
+    println(yearOfBirth)
+    var weight= 60
+    println(weight)
+    var phoneNumber="0722809298"
+    println(phoneNumber)
+
+}
